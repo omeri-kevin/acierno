cfff35860c64c37909665121e9a99838680fda9b
omeri-kevin omeri <omeri.kevin@iisgalvanimi.edu.it>
2025-02-14 12:01:06 +0000

    buon sanvalentino

--- file.py
+++ file.py
@@ -1,2 +1,12 @@
-print(0)
-print(9)
+inventario = {
+0 : {"nome": "Il PC di ...", "quantità": 1, "prezzo": 3999},
+1 : {"nome": "Nintendo Switch 2", "quantità": 10, "prezzo": 449},
+2 : {"nome": "GTA VI", "quantità": 3, "prezzo": 100}
+}
+def aggiunzione():
+    chiave = len(inventario) + 1 
+    nome = input("inserisci in nome")
+    quantità = (input("inserisci la quantità"))
+    prezzo = int(input("inserisci iil prezzo"))
+    invenrtario[chiave] = {"nome": nome , "quantità" :quantità , "prezzo" : prezzo}
+    print(aggiunzione)
